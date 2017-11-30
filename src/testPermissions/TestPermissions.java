a0eb6442558d5f65787592e785640c24b63732dd
yuehongjun <hongjun9817@126.com>
2017-11-30 11:30:01 +0800

    测试权限案例

---
+++ src/testPermissions/TestPermissions.java
@@ -0,0 +1,10 @@
+package testPermissions;
+
+public class TestPermissions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" 测试权限方法");
+	}
+
+}
